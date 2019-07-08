0f3f9fa0da2e67dbc7d7b7a3c433c823586e4705
张松 <1026905322@qq.com>
2019-07-08 16:23:55 +0800

    eureka注册中心demo

---
+++ eureka-service/src/main/java/com/zhangs/eurekaservice/EurekaServiceApplication.java
@@ -0,0 +1,18 @@
+package com.zhangs.eurekaservice;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.netflix.eureka.server.EnableEurekaServer;
+
+/**
+ * 此项目是注册中心
+ */
+@SpringBootApplication
+@EnableEurekaServer
+public class EurekaServiceApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(EurekaServiceApplication.class, args);
+	}
+
+}
